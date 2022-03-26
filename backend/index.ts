40ef743e5274c31baac5f5aea19c353aedf0bdaa
allisonvinicius <allisonvinicius63@gmail.com>
2022-03-26 14:29:47 -0400

    inital project

---
+++ backend/index.ts
@@ -0,0 +1,20 @@
+import { ApolloServer } from "apollo-server";
+import path from "path";
+import { buildSchema } from "type-graphql";
+
+async function main() {
+  const schema = await buildSchema({
+    resolvers: [],
+    emitSchemaFile: path.resolve(__dirname, "schema.gql"),
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  const { url } = await server.listen();
+
+  console.log(`$erver running on ${url}`);
+}
+
+main();
