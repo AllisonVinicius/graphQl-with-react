5a3576c10181abc9b1bb770a9d330f52a85ea33b
allisonvinicius <allisonvinicius63@gmail.com>
2022-04-04 23:01:00 -0400

    config

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,14 @@
 import { ApolloServer } from "apollo-server";
 import path from "path";
+import "reflect-metadata";
 import { buildSchema } from "type-graphql";
+import { UserResolver } from "./src/resolvers/UserResolver";
 
 async function main() {
   const schema = await buildSchema({
-    resolvers: [],
+    //resolvers => parecido com controllers, rotas da aplicação basicamente
+    //os resolvert fala para onde direcionar
+    resolvers: [UserResolver],
     emitSchemaFile: path.resolve(__dirname, "schema.gql"),
   });
 
--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,6 +1,6 @@
-import { Query } from "type-graphql";
+import { Query, Resolver } from "type-graphql";
 
-@Resolver
+@Resolver()
 export class UserResolver {
   @Query(() => String)
   async hello() {
