83cda1a0567d21929f3ceaadda2f1bef76b4649f
allisonvinicius <allisonvinicius63@gmail.com>
2022-04-10 19:14:22 -0400

    create, query and mutation

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,9 +1,23 @@
-import { Query, Resolver } from "type-graphql";
+import { Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../model/User";
 
+//query: buscar dados
+//mutation: criar,alterar, ou deletar dados
 @Resolver()
-export class UserResolver {
-  @Query(() => String)
-  async hello() {
-    return "Hello word Alliosn Dev pleno";
+export class UserResoler {
+  private data: User[] = [];
+
+  @Query(() => [User])
+  async users() {
+    return this.data;
+  }
+
+  @Mutation(() => User)
+  async createUser() {
+    const user = { id: "1", name: "Allison" };
+
+    this.data.push(user);
+
+    return this.users;
   }
 }
